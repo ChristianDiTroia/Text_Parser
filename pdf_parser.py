96ea85ec50a0b6ef0cbc7fcd3928c8cc276a31f3
Christian DiTroia <christian.ditroia@outlook.com>
2025-02-10 23:00:08 -0500

    Enhanced a bunch of stuff, especially newline and sentence parsing/regex quality

--- pdf_parser.py
+++ pdf_parser.py
@@ -21,16 +21,30 @@ def parse_pdf_text(
             extracted_text += cropped_page.extract_text() + "\n"
     return extracted_text
 
-def parse_by_sentence(text : str):
-    pattern = re.compile(r"(?<!\.)\.(?:\s|’)")
-    return re.split(pattern, text)
+# Removes periods from common titles to prevent parsing as a sentence
+def protect_abbreviations(text: str) -> str:
+    common_titles = re.compile(r"Mrs(\.\s?)|Mr(\.\s?)|Ms(\.\s?)|Dr(\.\s?)|Jr(\.\s?)|Sr(\.\s?)")
+    return re.sub(common_titles, '\\1', text)
 
-def parse_by_line(text : str):
-    pattern = re.compile(r"(?<!-)\n")
-    return re.split(pattern, text)
+def parse_by_sentence(text: str) -> list[str]:
+    sentence_delimiters = re.compile(r"(?<!\.|[A-Z])[\.?!](?:\s|[\"'’”])")
+    text = protect_abbreviations(text)
+    return re.split(sentence_delimiters, text)
+
+def concat_hyphenated_words(text: str) -> str:
+    hyphens = [r'-', r'–', r'—']
+    for hyphen in hyphens:
+        hyphenated = re.compile(hyphen + r"\n")
+        text = re.sub(hyphenated, hyphen, text)
+    return text
+
+def parse_by_line(text: str) -> list[str]:
+    text = concat_hyphenated_words(text)
+    newline = re.compile(r"\n")
+    return re.split(newline, text)
 
 def main():
-    parsed_gatsby = parse_by_sentence(parse_pdf_text("./the-great-gatsby.pdf"))
+    parsed_gatsby = parse_by_sentence(parse_pdf_text('./the-great-gatsby.pdf'))
     for line in parsed_gatsby:
         print(line)
     print(len(parsed_gatsby))
