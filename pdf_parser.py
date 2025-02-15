c7ac6651ba6b5ead02e0fd43d3a8bf3240c5d6b9
Christian DiTroia <christian.ditroia@outlook.com>
2025-02-15 16:25:00 -0500

    fixed a bunch of stuff

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,64 +1,55 @@
+from io import BytesIO
 import pdfplumber
 import re
 
-def parse_pdf_text(
-        pdf_path : str,
-        start_page : int = 0,
-        end_page : int = None,
-        crop_top: int = 50,
-        crop_bottom : int = 75
-        ):
-    extracted_text = ""
-    with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages[start_page-1:end_page]:
-            width, height = page.width, page.height
-            cropped_page = page.within_bbox((
-                0, 
-                crop_top, 
-                width, 
-                height - crop_bottom
-            ))
-            extracted_text += cropped_page.extract_text() + "\n"
-    return extracted_text
+class pdf_parser():
 
-# Removes periods from common titles to prevent parsing as a sentence
-def protect_abbreviations(text: str) -> str:
-    common_titles = re.compile(r"Mrs(\.\s?)|Mr(\.\s?)|Ms(\.\s?)|Dr(\.\s?)|Jr(\.\s?)|Sr(\.\s?)")
-    return re.sub(common_titles, '\\1', text)
+    def __init__(self, pdf_path: str):
+        self.pdf = self.__read_pdf(pdf_path)
+        self.parsed_sentences = ''
+        self.parsed_lines = ''
 
-def parse_by_sentence(text: str) -> list[str]:
-    sentence_delimiters = re.compile(r"(?<!\.|[A-Z])[\.?!](?:\s|[\"'’”])")
-    text = protect_abbreviations(text)
-    return re.split(sentence_delimiters, text)
-
-def concat_hyphenated_words(text: str) -> str:
-    hyphens = [r'-', r'–', r'—']
-    for hyphen in hyphens:
-        hyphenated = re.compile(hyphen + r"\n")
-        text = re.sub(hyphenated, hyphen, text)
-    return text
-
-def parse_by_line(text: str) -> list[str]:
-    text = concat_hyphenated_words(text)
-    newline = re.compile(r"\n")
-    return re.split(newline, text)
-
-# def parse_by_line(text : str):
-#     hyphen_newline_pattern = re.compile(r"(-\n\w*(\s|\.)\s)")
-#     line_pattern = re.compile(r"(?<!-)\n")
-#     # Concatenate words that span across lines to the same line
-#     for match in re.finditer(hyphen_newline_pattern, text):
-#         text = text[:match.end() - 1] + "\n" + text[match.end():] # Replace whitespace after each match with \n
-#         matched_str = match.group(0)
-#         print(matched_str)
-#         # text = text.replace(matched_str, matched_str.replace("\n", ""))
-#     return re.split(line_pattern, text)
-
-def main():
-    parsed_gatsby = parse_by_sentence(parse_pdf_text('./the-great-gatsby.pdf'))
-    for line in parsed_gatsby:
-        print(line)
-    print(len(parsed_gatsby))
+    def __read_pdf(pdf_path: str):
+        with open(pdf_path, 'rb') as file:
+            bytes = file.read()
+        return BytesIO(bytes) # read bytes into memory buffer and return the buffer
     
-if __name__ == '__main__':
-    main()
+    def __parse_pdf_text(
+            pdf_bytes: BytesIO,
+            start_page: int = 0,
+            end_page: int = None,
+            crop_top: int = 50,
+            crop_bottom: int = 75
+            ):
+        extracted_text = ""
+        with pdfplumber.open(pdf_bytes) as pdf:
+            for page in pdf.pages[start_page-1:end_page]:
+                width, height = page.width, page.height
+                cropped_page = page.within_bbox((
+                    0, 
+                    crop_top, 
+                    width, 
+                    height - crop_bottom
+                ))
+                extracted_text += cropped_page.extract_text() + "\n"
+        return extracted_text
+
+    # Removes periods from common titles to prevent parsing as a sentence
+    def __protect_abbreviations(self, text: str) -> str:
+        common_titles = re.compile(r"Mrs(\.\s?)|Mr(\.\s?)|Ms(\.\s?)|Dr(\.\s?)|Jr(\.\s?)|Sr(\.\s?)")
+        return re.sub(common_titles, '\\1', text)
+
+    def parse_sentences(self, text: str) -> list[str]:
+        sentence_delimiters = re.compile(r"(?<!\.|[A-Z])[\.?!](?:\s|[\"'’”])")
+        text = self.__protect_abbreviations(text)
+        self.parsed_sentences = re.split(sentence_delimiters, text)
+
+    # Concatenates words that are hyphenated across line breaks
+    def __concat_hyphenated_words(self, text : str) -> str:
+        hyphenated_word = re.compile(r"(-|–|—)\n(\w*)(\s|\.)?")
+        return re.sub(hyphenated_word, r"\g<2>\n", text)
+
+    def parse_lines(self, text: str) -> list[str]:
+        text = self.__concat_hyphenated_words(text)
+        newline = re.compile(r"\n")
+        self.parsed_sentences = re.split(newline, text)
